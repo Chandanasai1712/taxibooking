e8759c34faf4d1c213f2529be6309d649a5f786c
Chandanasai1712 <chandanasai.rpr@gmail.com>
2025-02-24 17:06:11 +0530

    fix: changed helm update.sh

--- helm_values_dockerconfigjson_update.sh
+++ helm_values_dockerconfigjson_update.sh
@@ -4,4 +4,4 @@ DOCKER_CONFIG_JSON=$1
 VALUES_FILE=$2
 
 # Update the dockerconfigjson field in values.yaml
-sed -i "s|dockerconfigjson: \"\"|dockerconfigjson: \"$DOCKER_CONFIG_JSON\"|" "$VALUES_FILE"
+sed -i "s|dockerconfigjson: \"\"|dockerconfigjson: \"$DOCKER_CONFIG_JSON\"|" "$VALUES_FILE"
